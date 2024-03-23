97a4c6f9739f2cadf8ca2af4377b2f7d29c87844
Stella Bloom <windowz414@gnuweeb.org>
2024-03-23 20:53:38 +0100

    lancelot: #KeepEvolving!
    Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>

--- evolution_lancelot.mk
+++ evolution_lancelot.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lancelot
+PRODUCT_NAME := evolution_lancelot
 PRODUCT_DEVICE := lancelot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
